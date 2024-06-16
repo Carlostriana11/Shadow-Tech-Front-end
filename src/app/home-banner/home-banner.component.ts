1bf7aa4b7381a4f2a876c9ca02d140202d1c5064
Juan Diego <juanndiiego2@gmail.com>
2024-06-15 21:18:48 -0500

    add banner Home-page

--- src/app/home-banner/home-banner.component.ts
+++ src/app/home-banner/home-banner.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home-banner', // Ajusta el selector aquí
+  selector: 'app-home-banner', 
   templateUrl: './home-banner.component.html',
   styleUrls: ['./home-banner.component.css']
 })
 export class HomeBannerComponent {
-  
-  scrollDown(): void {
-    const banner = document.getElementById('banner');
-    if (banner) {
-      banner.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }
+
   }
-}
